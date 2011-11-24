195e77f90daf98806690d4a9fb2186f365314bf9
Steve Howell <steve@strings.com>
2011-11-24 09:10:50 -0800

    extract helpers

--- demo.coffee
+++ demo.coffee
@@ -43,66 +43,7 @@ run_code = (code) ->
     console.log "problem in JS"
     
 prelude = '''
-  environment = ->
-    width = 500
-    ns = 'http://www.w3.org/2000/svg'
-    canvas = ->
-      svg = document.createElementNS ns, 'svg'
-      svg.width = width
-      svg.height = width
-      svg.style.height = width + 'px'
-      svg.style.width = width + 'px'
-      svg.style.border = "1px black solid"
-      elem = document.getElementById("main")  
-      $(elem).html svg
-      svg
-    
-    svg = canvas()
-    
-    cosine = (angle) ->
-      Math.cos (angle * Math.PI / 180)
-      
-    sine = (angle) ->
-      Math.sin (angle * Math.PI / 180)
-  
-    ellipse = (cx=width/2, cy=width/2, rx=15, ry=15, fill="blue") ->
-      dot = document.createElementNS ns, 'ellipse'
-      dot.setAttribute 'rx', rx
-      dot.setAttribute 'ry', ry
-      dot.setAttribute 'cx', cx
-      dot.setAttribute 'cy', cy
-      dot.setAttribute "fill", fill
-      svg.appendChild dot
-      dx = 5
-      dy = 0
-      self =
-        attr: (field, value) ->
-          dot.setAttribute field, value
-        move: ->
-          cx += dx
-          cy += dy
-          n = Math.floor cx / width
-          self.attr "cx", cx
-          self.attr "cy", cy
-          ellipse(cx, cy, 1, 1, "red")
-        turn: (angle) ->
-          c = cosine(angle)
-          s = sine(angle)
-          [dx, dy] = [dx * c + dy * s, dy * c - dx * s]
-
-    after = (t, f) -> setTimeout f, t
-
-    repeat = (f) ->
-      f()
-      after 100, -> repeat(f)
-
-    svg: svg
-    ellipse: ellipse
-    circle: ellipse
-    width: width
-    repeat: repeat
-    
-  env = environment()
+  env = window.helpers()
   {circle, repeat} = env
   ''' + '\n'
   
---
+++ helpers.js
@@ -0,0 +1,89 @@
+(function() {
+  window.helpers = function() {
+    var after, canvas, cosine, ellipse, ns, repeat, sine, svg, width;
+    width = 500;
+    ns = 'http://www.w3.org/2000/svg';
+    canvas = function() {
+      var elem, svg;
+      svg = document.createElementNS(ns, 'svg');
+      svg.width = width;
+      svg.height = width;
+      svg.style.height = width + 'px';
+      svg.style.width = width + 'px';
+      svg.style.border = "1px black solid";
+      elem = document.getElementById("main");
+      $(elem).html(svg);
+      return svg;
+    };
+    svg = canvas();
+    cosine = function(angle) {
+      return Math.cos(angle * Math.PI / 180);
+    };
+    sine = function(angle) {
+      return Math.sin(angle * Math.PI / 180);
+    };
+    ellipse = function(cx, cy, rx, ry, fill) {
+      var dot, dx, dy, self;
+      if (cx == null) {
+        cx = width / 2;
+      }
+      if (cy == null) {
+        cy = width / 2;
+      }
+      if (rx == null) {
+        rx = 15;
+      }
+      if (ry == null) {
+        ry = 15;
+      }
+      if (fill == null) {
+        fill = "blue";
+      }
+      dot = document.createElementNS(ns, 'ellipse');
+      dot.setAttribute('rx', rx);
+      dot.setAttribute('ry', ry);
+      dot.setAttribute('cx', cx);
+      dot.setAttribute('cy', cy);
+      dot.setAttribute("fill", fill);
+      svg.appendChild(dot);
+      dx = 5;
+      dy = 0;
+      return self = {
+        attr: function(field, value) {
+          return dot.setAttribute(field, value);
+        },
+        move: function() {
+          var n;
+          cx += dx;
+          cy += dy;
+          n = Math.floor(cx / width);
+          self.attr("cx", cx);
+          self.attr("cy", cy);
+          return ellipse(cx, cy, 1, 1, "red");
+        },
+        turn: function(angle) {
+          var c, s, _ref;
+          c = cosine(angle);
+          s = sine(angle);
+          return _ref = [dx * c + dy * s, dy * c - dx * s], dx = _ref[0], dy = _ref[1], _ref;
+        }
+      };
+    };
+    after = function(t, f) {
+      return setTimeout(f, t);
+    };
+    repeat = function(f) {
+      f();
+      return after(100, function() {
+        return repeat(f);
+      });
+    };
+    return {
+      svg: svg,
+      ellipse: ellipse,
+      circle: ellipse,
+      width: width,
+      repeat: repeat
+    };
+  };
+}).call(this);
